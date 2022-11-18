e4a4dfd34579d0200740b077c4e8235936ae4e24
CES_POC <117299732+CES-POC@users.noreply.github.com>
2022-11-18 23:58:20 +0530

    Update axios.js

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-    baseURL: 'https://neobck.azurewebsites.net'
+    baseURL: 'https://neobck.azurewebsites.net/'
 });
 
 export default axiosConfig
